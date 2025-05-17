4b2a9e83229742013dce3061aca2bbdcff6bca52
Arturo Pérez <arjperez31@gmail.com>
2025-05-17 15:50:55 -0400

    adding src folder to project

--- src/core/exportCVS.js
+++ src/core/exportCVS.js
@@ -1,13 +1,13 @@
 import { writeFileSync } from 'fs';
 export const exportCVS = async (supabaseClient) => {
     try {
-        
+
         const { data } = await supabaseClient.from('tweets_nosql').select('tweet_data, username');
         const csv = data.map(row => {
             const d = row.tweet_data;
             return `${d.tweet_id},${row.username},${d.text.replace(/,/g, '')},${d.created_at}`;
         }).join('\n');
-        writeFileSync('tweets.csv', `tweet_id,username,text,created_at\n${csv}`);
+        writeFileSync('../data/tweets.csv', `tweet_id,username,text,created_at\n${csv}`);
         console.log('Datos exportados a tweets.csv');
     } catch (error) {
         console.error('Error exportando a CSV:', error.message);
