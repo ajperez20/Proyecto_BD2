f477a0fa53c35151fcaa4f2ec82927f14d9478de
Jrgil.20 <131034722+Jrgil20@users.noreply.github.com>
2025-06-29 21:40:21 -0400

    feat: actualizar la exportación a CSV para utilizar csv-writer y mejorar la estructura de datos

--- src/core/exportCVS.js
+++ src/core/exportCVS.js
@@ -1,15 +1,32 @@
-import { writeFileSync } from 'fs';
+import { createObjectCsvWriter } from 'csv-writer';
 import { join } from 'path';
+
 export const exportCVS = async (supabaseClient) => {
     try {
         const { data } = await supabaseClient.from('tweets_nosql').select('tweet_data, username');
-        const csv = data.map(row => {
-            const d = row.tweet_data;
-            return `${d.tweet_id},${row.username},${d.text.replace(/,/g, '')},${d.created_at}`;
-        }).join('\n');
-        // Guardar el archivo en extraction/tweets.csv
+
         const outputPath = join(process.cwd(), 'tweets.csv');
-        writeFileSync(outputPath, `tweet_id,username,text,created_at\n${csv}`);
+        const csvWriter = createObjectCsvWriter({
+            path: outputPath,
+            header: [
+                { id: 'tweet_id', title: 'tweet_id' },
+                { id: 'username', title: 'username' },
+                { id: 'text', title: 'text' },
+                { id: 'created_at', title: 'created_at' },
+            ]
+        });
+
+        const records = data.map(row => {
+            return {
+                tweet_id: row.tweet_data.tweet_id,
+                username: row.username,
+                text: row.tweet_data.text,
+                created_at: row.tweet_data.created_at
+            }
+        });
+
+        await csvWriter.writeRecords(records);
+
         console.log(`Datos exportados a ${outputPath}`);
     } catch (error) {
         console.error('Error exportando a CSV:', error.message);
